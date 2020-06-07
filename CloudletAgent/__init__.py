af1e34cc7f1fa100e09f96611046a0427c3a4aea
Никита Белокриницкий <no.whale@ya.ru>
2020-06-07 19:35:44 +0500

    Fixed path detection to a file with cloudlet information

--- CloudletAgent/__init__.py
+++ CloudletAgent/__init__.py
@@ -5,4 +5,4 @@ import pathlib
 docker_client = docker.from_env()
 cloudlet_info = {}
 cloudlets_service_url = os.environ.get('CLOUDLETS_SERVICE_URL')
-cloudlet_info_file_path = str(pathlib.Path().absolute()) + '/cloudlet_info.txt'
+cloudlet_info_file_path = str(pathlib.Path(__file__).parent.absolute()) + '/cloudlet_info.txt'
